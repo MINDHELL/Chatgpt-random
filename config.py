03293ef5e89ba0cbf5edb508291f624cebe2c770
MINDHELL <Tgpurpose640@gmail.com>
2025-01-29 20:27:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,3 +5,4 @@ API_HASH = os.getenv("API_HASH", "9df7e9ef3d7e4145270045e5e43e1081")  # Replace 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "7692429836:AAHyUFP6os1A3Hirisl5TV1O5kArGAlAEuQ")  # Replace with your Bot Token
 DATABASE_CHANNEL = int(os.getenv("DATABASE_CHANNEL", "-1002465297334"))  # Replace with your channel ID
 MONGO_DB_URI = "mongodb+srv://aarshhub:6L1PAPikOnAIHIRA@cluster0.6shiu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+SEND_IMAGES_AND_VIDEOS = "True"
