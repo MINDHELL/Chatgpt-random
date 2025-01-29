1e979090a3d1213e5e9eb9e2b6f1ae66d9f3093c
MINDHELL <Tgpurpose640@gmail.com>
2025-01-29 15:03:22 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,6 +1,6 @@
 import os
 
-API_ID = int(os.getenv("API_ID", "123456"))  # Replace with your API ID
-API_HASH = os.getenv("API_HASH", "your_api_hash")  # Replace with your API Hash
-BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")  # Replace with your Bot Token
-DATABASE_CHANNEL = int(os.getenv("DATABASE_CHANNEL", "-1001234567890"))  # Replace with your channel ID
+API_ID = int(os.getenv("API_ID", "27788368"))  # Replace with your API ID
+API_HASH = os.getenv("API_HASH", "9df7e9ef3d7e4145270045e5e43e1081"")  # Replace with your API Hash
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7692429836:AAHyUFP6os1A3Hirisl5TV1O5kArGAlAEuQ")  # Replace with your Bot Token
+DATABASE_CHANNEL = int(os.getenv("DATABASE_CHANNEL", "-1002465297334"))  # Replace with your channel ID
